3b087d459407d8b5d3c7e2c08204d64f26ab85a2
Helmut Michael Juskewycz <helmut@lingohub.com>
2025-05-07 11:39:47 +0200

    adds publish for maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,67 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.signing)
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+group = "com.lingohub.android.cdn"
+version = "1.0.0"
+
+android {
+    namespace = "com.lingohub.android.cdn"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["release"])
+
+            pom {
+                name.set("Lingohub Android CDN SDK")
+                description.set("A lightweight Android SDK that retrieves up-to-date translations from Lingohub, enabling real-time multilingual content delivery without requiring app updates.")
+                url.set("https://github.com/lingohub/android-cdn-sdk")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("lingohub")
+                        name.set("lingohub GmbH")
+                        email.set("office@lingohub.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/lingohub/android-cdn-sdk.git")
+                    developerConnection.set("scm:git:git@github.com:lingohub/android-cdn-sdk.git")
+                    url.set("https://github.com/lingohub/android-cdn-sdk")
+                }
+            }
+        }
     }
 }
